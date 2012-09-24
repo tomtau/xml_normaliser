1bddec9fb7429d21e9c9781fc397fcb7b8a63a66
TomT <s0943263@sms.ed.ac.uk>
2012-09-24 11:44:07 +0100

    Created a basic Java project structure

---
+++ src/uk/ac/ed/inf/proj/xmlnormaliser/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * XML Normalisation according to 'A Normal Form for XML Documents' (XNF) by Libkin and Arenas
+ */
+/**
+ * The root package
+ * @author Tomas Tauber
+ *
+ */
+package uk.ac.ed.inf.proj.xmlnormaliser;
---
+++ tests/uk/ac/ed/inf/proj/xmlnormaliser/parser/dtd/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * XML Normalisation according to 'A Normal Form for XML Documents' (XNF) by Libkin and Arenas
+ */
+/**
+ * DTD Parsing tests
+ * @author Tomas Tauber
+ *
+ */
+package uk.ac.ed.inf.proj.xmlnormaliser.parser.dtd;
