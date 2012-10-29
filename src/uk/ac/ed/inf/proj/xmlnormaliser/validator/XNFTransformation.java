acb1c8be7cbcac96a9713250333ff62502994124
TomT <s0943263@sms.ed.ac.uk>
2012-10-29 01:45:50 +0000

    Returning actions for creating new attribute types
    Partially (transformations of original XFDs not yet done) -- needs more
    test cases

--- src/uk/ac/ed/inf/proj/xmlnormaliser/validator/XNFTransformation.java
+++ src/uk/ac/ed/inf/proj/xmlnormaliser/validator/XNFTransformation.java
@@ -25,7 +25,7 @@ public class XNFTransformation {
 	 * @param path set of paths
 	 * @return elements of the longest path
 	 */
-	private static String[] getLastElements(FDPath path) {
+	private static String[] getLongestLastElements(FDPath path) {
 		String[] last = {};
 		int max = 0;
 		for (String p : path) {
@@ -52,7 +52,7 @@ public class XNFTransformation {
 	 */
 	public static List<TransformAction> moveAttribute(FDPath leftHandSide, String rightHandSide, HashMap<FDPath, FDPath> originalXfds, DTD doc) {
 		List<TransformAction> actions = new ArrayList<TransformAction>();
-		String[] q = getLastElements(leftHandSide);
+		String[] q = getLongestLastElements(leftHandSide);
 		int qIndex = q.length - 1;
 		if (q[qIndex].startsWith("@")) {
 			qIndex -= 1;
@@ -93,16 +93,89 @@ public class XNFTransformation {
 		
 		return actions;
 	}
+
+	/**
+	 * Return the last element of the non-attribute path
+	 * @param path set of paths
+	 * @return last element of the non-attribute path (or null if no such path found)
+	 */
+	private static String[] getQElements(FDPath path) {
+		for (String p : path) {
+			if (!p.contains("@")) {
+				return p.split("\\.");
+			}
+		}
+		return null;
+	}	
+
+	/**
+	 * Return the last element of the attribute paths
+	 * @param path set of paths
+	 * @return last element of the non-attribute paths (or null if no such path found)
+	 */
+	private static String[][] getPElements(FDPath path) {
+		String[][] result = new String[path.size() - 1][];
+		int index = 0;
+		for (String p : path) {
+			if (p.contains("@")) {
+				result[index] = p.split("\\.");
+				index++;
+			}
+		}
+		return result;
+	}	
+	
 	
 	/**
-	 * 
+	 * Given an anomalous XFD of the form q, p1.@l1, p1.@l2, p1.@l3..., pn.@ln -> p.@l, the set of actions to transform DTD and XFDs is returned
 	 * @param leftHandSide
 	 * @param rightHandSide
 	 * @param originalXfds
 	 * @param doc
 	 * @return
 	 */
-	public static List<TransformAction> createNewET(int etCount, FDPath leftHandSide, String rightHandSide, HashMap<FDPath, FDPath> originalXfds, DTD doc) {
-		return new ArrayList<TransformAction>();
+	public static List<TransformAction> createNewET(int exCount, String namePrefix, FDPath leftHandSide, String rightHandSide, HashMap<FDPath, FDPath> originalXfds, DTD doc) {
+		List<TransformAction> actions = new ArrayList<TransformAction>();
+		String[] q = getQElements(leftHandSide);
+		String lastQ = q[q.length - 1];
+		
+		StringBuilder qPath = new StringBuilder(q[0]);
+		for (int i = 1; i < q.length; i++) {
+			qPath.append(".").append(q[i]);
+		}
+		
+		actions.add(new TransformAction(TransformAction.ActionType.ADD_NODE, new Object[] {lastQ, namePrefix + exCount}));
+		String[][] keys = getPElements(leftHandSide);
+		for (int innerCount = 0; innerCount < keys.length; innerCount++) {
+			actions.add(new TransformAction(TransformAction.ActionType.ADD_NODE, new Object[] {namePrefix + exCount, (namePrefix + exCount) + innerCount}));
+		}
+		String[] p = rightHandSide.split("\\.");
+		actions.add(new TransformAction(TransformAction.ActionType.DELETE_NODE, new Object[] {p[p.length - 3], p[p.length - 2]}));
+		actions.add(new TransformAction(TransformAction.ActionType.ADD_NODE, new Object[] {namePrefix + exCount, p[p.length - 2]}));
+		int innerCount = 0;
+		for (String[] pn : keys) {
+			actions.add(new TransformAction(TransformAction.ActionType.ADD_ATTRIBUTE, new Object[] {(namePrefix + exCount) + innerCount, pn[pn.length - 1]}));
+			innerCount++;
+		} 
+		actions.add(new TransformAction(TransformAction.ActionType.DELETE_XFD, new Object[] {leftHandSide, new FDPath(rightHandSide)}));
+		
+		/* conversion of original XFDs - not yet implemented */
+		
+		String qp = qPath.toString();
+		String qpN = qp + "." + namePrefix + exCount;
+		for (innerCount = 0; innerCount < keys.length; innerCount++) {
+			FDPath lhs1 = new FDPath(qpN);
+			FDPath lhs2 = new FDPath(qp);
+			String current = qpN + "." + (namePrefix + exCount) + innerCount;
+			for (String[] pn : keys) {
+				lhs1.add(current + "." + pn[pn.length - 1]);
+				lhs2.add(current + "." +pn[pn.length - 1]);
+			}
+			actions.add(new TransformAction(TransformAction.ActionType.ADD_XFD, new Object[] {lhs1, new FDPath(current)}));
+			actions.add(new TransformAction(TransformAction.ActionType.ADD_XFD, new Object[] {lhs2, new FDPath(qpN)}));
+			
+		}
+		
+		return actions;
 	}	
 }
